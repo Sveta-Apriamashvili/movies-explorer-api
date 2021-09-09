989bda2db6fb23f18edb63b91487150e64ba6405
Sveta Apriamashvili <s.v.apriamashvili@yandex.com>
2021-09-09 16:25:30 +1000

    fix rout updateUser

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ const {
 router.get('/users/me', getUser);
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required,
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().custom(method),
   }),
 }), updateUser);

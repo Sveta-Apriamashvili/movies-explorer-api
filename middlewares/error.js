46c5096f44eb99e83963c6cf1fa12db72cf4288a
Sveta Apriamashvili <s.v.apriamashvili@yandex.com>
2021-09-11 19:17:19 +1000

    check errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,7 @@
 const ErrorFormatter = require('../utils/error_formatter');
 
 const errorsHandler = (err, req, res, next) => {
+  console.log(`name: ${err.name}; code: ${err.code}`)
   const error = ErrorFormatter.formatError(err);
   const status = error.statusCode;
   const { message } = error;
